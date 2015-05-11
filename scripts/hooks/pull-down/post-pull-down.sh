c9108922d7dc810f69b6aa4a1742fea219f44c34
btopro <bto108@psu.edu>
2015-05-11 16:20:42 -0400

    post pull down hook updated

--- scripts/hooks/pull-down/post-pull-down.sh
+++ scripts/hooks/pull-down/post-pull-down.sh
@@ -2,6 +2,6 @@
 
 # disable modules that will absolutely cause issues if there's
 # a significant environment change from prod to dev
-drush @elmsln dis securepages apc filecache apdqc --y
+drush $2 dis securepages apc filecache apdqc --y
 # cook the go local script
-drush @elmsln cook golocal --mlt-email_address=$1 --y
+drush $2 cook golocal --mlt-email_address=$1 --y
