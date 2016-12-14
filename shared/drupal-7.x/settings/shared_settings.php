2b43b9e83784eddd377a0de3b2405d460fbbfeeb
btopro <btopro@outlook.com>
2016-12-14 09:21:17 -0500

    example setting disabled

--- shared/drupal-7.x/settings/shared_settings.php
+++ shared/drupal-7.x/settings/shared_settings.php
@@ -22,3 +22,6 @@ $conf['environment_indicator_overwritten_color'] = '#42b96a';
 
 // enable this to support the legacy zurb foundation components
 //$conf['foundation_access_legacy'] = TRUE;
+
+// settings for mariadb with APDQC
+//$databases['default']['default']['mysql_db_type'] = 'mariadb';
